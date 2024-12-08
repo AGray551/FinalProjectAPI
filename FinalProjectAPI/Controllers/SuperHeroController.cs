8d4e088c455520d97c8a7bd540458bd5b770bf13
AGray551 <gray.andrews.ga@gmail.com>
2024-12-08 18:34:03 -0500

    Commit 2

--- FinalProjectAPI/Controllers/SuperHeroController.cs
+++ FinalProjectAPI/Controllers/SuperHeroController.cs
@@ -45,13 +45,37 @@ namespace FinalProjectAPI.Controllers
 
             return Ok(await _context.SuperHeroes.ToListAsync());
         }
+
+
         [HttpPut]
-        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatedHero)
         {
-            _context.SuperHeroes.Add(hero);
+            var dbHero = await _context.SuperHeroes.FindAsync(updatedHero.Id);
+            if (dbHero == null)
+                return NotFound("Hero not found");
+
+            dbHero.Name = updatedHero.Name;
+            dbHero.FirstName = updatedHero.FirstName;
+            dbHero.LastName = updatedHero.LastName;
+            dbHero.Place = updatedHero.Place;
+
+            await _context.SaveChangesAsync(); 
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+            if (dbHero == null)
+                return NotFound("Hero not found");
+
+            _context.SuperHeroes.Remove(dbHero);   
             await _context.SaveChangesAsync();
 
             return Ok(await _context.SuperHeroes.ToListAsync());
         }
+
     }
 }
